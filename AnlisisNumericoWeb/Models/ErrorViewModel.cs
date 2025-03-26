9b5a53d0ff510ebf88eeaa1d2bd00e719ad4a405
hernanmaldo <hernannmaldo@gmail.com>
2025-03-26 18:45:23 -0300

    InitialCreate

---
+++ AnlisisNumericoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnlisisNumericoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
